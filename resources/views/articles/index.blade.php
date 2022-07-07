ccee219a34a01ab50c0a0a411090274b458aa2cb
Justin-Pop123 <swanhtetnaing714@gmail.com>
2022-07-07 16:13:35 +0630

    review prepare

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,29 +1,28 @@
-@extends("layouts.app")
+@extends('layouts.app')
 
-@section("content")
-  <div class="container">
-    @if(session('info'))
-        <div class="alert alert-info">
-            {{ session('info') }}
-        </div>
-    @endif
+@section('content')
+    <div class="container">
+        @if (session('info'))
+            <div class="alert alert-info">
+                {{ session('info') }}
+            </div>
+        @endif
 
-    {{ $articles->links() }}
+        {{ $articles->links() }}
 
-    @foreach($articles as $article)
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">{{ $article->title }}</h5>
-          <div class="card-subtitle mb-2 text-muted small">
-            {{ $article->created_at->diffForHumans() }}
-          </div>
-          <p class="card-text">{{ $article->body }}</p>
-          <a class="card-link btn btn-primary"
-            href="{{ url("/articles/detail/$article->id") }}">
-            View Detail &raquo;
-          </a>
-        </div>
-      </div>
-    @endforeach
-  </div>
+        @foreach ($articles as $article)
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $article->title }}</h5>
+                    <div class="card-subtitle mb-2 text-muted small">
+                        {{ $article->created_at->diffForHumans() }}
+                    </div>
+                    <p class="card-text">{{ $article->body }}</p>
+                    <a class="card-link btn btn-primary" href="{{ url("/articles/detail/$article->id") }}">
+                        View Detail &raquo;
+                    </a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
